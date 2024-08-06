23ebc83ce620bf05b59d796c59cf68e962715778
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-08-06 21:06:08 +0400

    Writing the code
    Used the open-source lib from the freecurrencyapi.com

--- currency api.py
+++ currency api.py
@@ -1,1 +1,14 @@
+import freecurrencyapi as curapi
 
+def exchange_rate(from_cur, to_cur, api_key):
+
+    client = curapi.Client(api_key)
+
+    result = client.latest(currencies=[from_cur, to_cur])
+    return result["data"][to_cur]
+
+from_cur = "USD"
+to_cur = "TRY"
+api_key = "" # your api key from freecurrencyapi.com (It is fully free!)
+print(f"Курс {from_cur} к {to_cur}:", end=" " )
+print(exchange_rate(from_cur, to_cur, api_key))
